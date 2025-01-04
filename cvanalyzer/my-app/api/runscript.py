9e40e3c2a93328ecb20cd9fc820b9596dfc041ac
Aditya <adityagautam0601@gmail.com>
2025-01-04 13:53:37 +0530

    v1.47

--- cvanalyzer/my-app/api/runscript.py
+++ cvanalyzer/my-app/api/runscript.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import subprocess
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 app = FastAPI()
 
 @app.post("/api/runscript")
